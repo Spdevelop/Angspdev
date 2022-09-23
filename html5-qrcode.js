30edca9cda8bf44e358e44c6dd054ed4be2820fd
spdevToneer <wheel_2538@hotmail.com>
2022-09-23 17:05:08 +0700

    rs

---
+++ html5-qrcode.js
@@ -0,0 +1,73 @@
+(function($) {
+    jQuery.fn.extend({
+        html5_qrcode: function(qrcodeSuccess, qrcodeError, videoError) {
+            return this.each(function() {
+                var currentElem = $(this);
+
+                var height = currentElem.height();
+                var width = currentElem.width();
+
+                if (height == null) {
+                    height = 250;
+                }
+
+                if (width == null) {
+                    width = 300;
+                }
+
+                var vidElem = $('<video width="' + width + 'px" height="' + height + 'px" autoplay playsinline></video>').appendTo(currentElem);
+                var canvasElem = $('<canvas id="qr-canvas" width="' + (width - 2) + 'px" height="' + (height - 2) + 'px" style="display:none;"></canvas>').appendTo(currentElem);
+
+                var video = vidElem[0];
+                var canvas = canvasElem[0];
+                var context = canvas.getContext('2d');
+                var localMediaStream;
+
+                var scan = function() {
+                    if (localMediaStream) {
+                        context.drawImage(video, 0, 0, 307, 250);
+
+                        try {
+                            qrcode.decode();
+                        } catch (e) {
+                            qrcodeError(e, localMediaStream);
+                        }
+
+                        $.data(currentElem[0], "timeout", setTimeout(scan, 500));
+
+                    } else {
+                        $.data(currentElem[0], "timeout", setTimeout(scan, 500));
+                    }
+                };//end snapshot function
+
+                window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
+
+
+                navigator.mediaDevices.getUserMedia({video: { facingMode: "environment" }}).then(function successCallback(stream) {
+                    video.srcObject = stream;
+                    localMediaStream = stream;
+                    $.data(currentElem[0], "stream", stream);
+
+                    video.play();
+                    $.data(currentElem[0], "timeout", setTimeout(scan, 1000));
+                }).then(function(error) {
+                    videoError(error, localMediaStream);
+                });
+
+                qrcode.callback = function (result) {
+                    qrcodeSuccess(result, localMediaStream);
+                };
+            }); // end of html5_qrcode
+        },
+        html5_qrcode_stop: function() {
+            return this.each(function() {
+                //stop the stream and cancel timeouts
+                $(this).data('stream').getVideoTracks().forEach(function(videoTrack) {
+                    videoTrack.stop();
+                });
+
+                clearTimeout($(this).data('timeout'));
+            });
+        }
+    });
+})(jQuery);
